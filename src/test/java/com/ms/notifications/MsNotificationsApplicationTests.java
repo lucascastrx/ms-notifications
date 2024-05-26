78e3f4ace310a179423eb2811416aa0a15ad3d28
Lucas <luukascastro@gmail.com>
2024-05-26 20:27:30 -0300

    Initial commit

---
+++ src/test/java/com/ms/notifications/MsNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
